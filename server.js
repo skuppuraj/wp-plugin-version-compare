bb63b56dc0c87849d8e7afcd88bdff51e074a9f3
Kuppuraj <kuppuraj@live.in>
2025-02-07 12:33:11 +0530

    Improve: Scraper.html added

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const shell = require('shelljs');
 
+const path = require('path');
+
 const app = express();
+app.use(express.json()); // Parse JSON requests
+
+// Serve scraper.html
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json()); // For parsing JSON body
 
 app.post('/run-npm', (req, res) => {
+    console.log(req.body);
     
     const { command } = req.body; // Expecting { "command": "install some-package" }
 
     if (!command) {
         return res.status(400).json({ error: 'No command provided' });
     }
+// console.log(command);
 
     // Run the command using shelljs
     const result = shell.exec(`${command}`, { silent: true });
